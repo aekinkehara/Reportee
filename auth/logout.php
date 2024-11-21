14caa547eb8da3824ecfdb929eb0be39d2453bba
aekinkehara <aekinkehara@gmail.com>
2024-11-21 10:31:02 +0700

    first commit

---
+++ auth/logout.php
@@ -0,0 +1,7 @@
+<?php
+// session = loginnya suatu user
+
+session_start();
+session_destroy();
+header('Location: login.php');
+?>
---
+++ masyarakat/laporan.php
@@ -0,0 +1,252 @@
+<?php
+
+session_start();
+
+include '../db/connect.php';
+include '../function/pengaduan.php';
+
+// cek yang login bener bener masrakat atau bukan
+if($_SESSION['role'] != 1){
+    header('Location: ../auth/login.php');
+    exit();
+}
+
+//proses input data
+if(isset($_POST['submit'])) {
+    // validasi input
+    $report = $_POST['message'] ?? '';
+
+    //
+    if(isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+        $image = $_FILES['image']['name'];
+        $tmp_name = $_FILES['image']['tmp_name']; 
+        $target_dir = "../uploads/";
+    
+        // Tambahkan tanda `$` di depan `image` pada bagian `move_uploaded_file`
+        move_uploaded_file($tmp_name, $target_dir . $image);
+    }else {
+        $image = '';
+    }
+    
+}
+
+//proses input ke database
+if(!empty($report)) {
+    $status = buat_pengaduan($_SESSION['user_id'], $report, $image, $conn); 
+
+    if($status) {
+        header("Location:dashboard-masyarakat.php"); 
+    }else {
+        echo "Terjadi kesalahan, coba masukkan data lagi."; // Tambahkan titik koma `;` di akhir echo
+    }
+}else {
+    // echo "Masukkan data terlebih dahulu."; // Tambahkan titik koma `;` di akhir echo
+}
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Report</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="../bootstrap/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../bootstrap/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fa-regular fa-face-smile-beam" style="color: #04ff00;"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Reportee</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="dashboard-masyarakat.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="laporan.php">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Buat Laporan</span></a>
+            </li>
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['username']?></span>
+                                <img class="img-profile rounded-circle" src="../bootstrap/img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="../auth/logout.php" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Form Buat Laporan</h1>
+                    </div>
+
+
+                    <!-- Content Row -->
+                    <div class="row">
+                        <!-- Area Chart -->
+                        <div class="col-xl-12 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-success">Silahkan masukan data-data yang dimintai</h6>
+
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <form class="user" method="POST" enctype="multipart/form-data">
+                                        <div class="form-group">
+                                            <label class="lable-control" for="username">Deskripsi laporan</label>
+                                            <textarea type="message" name="message" class="form-control" placeholder="Tuliskan deskripsi"></textarea>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="lable-control" for="password">Bukti Foto</label>
+                                            <input type="file" name="image" class="form-control" placeholder="Masukkan Foto di sini">
+                                        </div>
+
+                                        <button type="submit" name="submit" class="btn btn-success">Submit</button>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-success">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto text-gray-100">
+                        <span>&copy; Reportee 2024</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="../auth/logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../bootstrap/vendor/jquery/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../bootstrap/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../bootstrap/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../bootstrap/js/demo/chart-area-demo.js"></script>
+    <script src="../bootstrap/js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
